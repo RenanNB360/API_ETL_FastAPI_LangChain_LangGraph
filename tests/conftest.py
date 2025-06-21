2424d37758725289fca1d209e599f572b9a8d0a7
Renan Nunes Bittencourt <programmerrnb@gmail.com>
2025-06-21 11:23:10 -0300

    Integrando com o SQLite

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,14 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
-from sqlalchemy import event
-from datetime import datetime
 
 from api_etl.internal_api.app import app
 from api_etl.internal_api.models import table_registry
-from contextlib import contextmanager
+
 
 @pytest.fixture
 def client():
@@ -25,9 +26,13 @@ def session():
 
 
 @contextmanager
-def _mock_db_time(model, time=datetime(2025, 6, 20)):
+def _mock_db_time(*, model, time=datetime(2025, 6, 21)):
     def fake_time_hook(mapper, connection, target):
-        print(target)
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
     event.listen(model, 'before_insert', fake_time_hook)
     yield time
     event.remove(model, 'before_insert', fake_time_hook)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,21 +1,27 @@
 from dataclasses import asdict
+from datetime import datetime
 
 from sqlalchemy import select
 
 from api_etl.internal_api.models import User
 
 
-def test_create_user(session):
-    new_user = User(username='test', position='tester', email='test@example.com', password='test123')
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User, time=datetime.now()) as time:
+        new_user = User(username='test', position='tester', email='test@example.com', password='test123')
 
-    session.add(new_user)
-    session.commit()
+        session.add(new_user)
+        session.commit()
+        user = session.scalar(select(User).where(User.username == 'test'))
 
-    user = session.scalar(select(User).where(User.username == 'test'))
-    assert asdict(user) == {
+    user_dict = asdict(user)
+    user_dict.pop('id')  # Remove o ID gerado automaticamente
+
+    assert user_dict == {
         'username': 'test',
         'position': 'tester',
         'email': 'test@example.com',
         'password': 'test123',
-        'created_at': ...,
+        'created_at': time,
+        'updated_at': time,
     }
